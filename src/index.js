d1974ac88c43af8b600ee0a8b7e67952573c4c4f
AswathyRajM <61063226+AswathyRajM@users.noreply.github.com>
2021-04-19 01:40:46 -0400

    middleware issue

--- src/index.js
+++ src/index.js
@@ -18,8 +18,7 @@ const store = createStore(
 	reducer,
 	compose(
 		applyMiddleware(thunk),
-		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-	)
+			)
 );
 
 ReactDOM.render(
